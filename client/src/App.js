546a15b85f8d73adfd0166a40e967912ced5679c
Eirik Enriquez <eirikdbbd@gmail.com>
2023-09-04 17:15:05 +1200

    changed favicon

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./components/Layout";
 import Play from "./pages/Play";
@@ -7,14 +7,14 @@ import About from "./pages/About";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Play />} />
           <Route path="/about" element={<About />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
