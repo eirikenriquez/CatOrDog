67d67f257bb57729ab2575c9bddcd97fba657e73
Eirik Enriquez <eirikdbbd@gmail.com>
2023-09-27 11:46:26 +1300

    added css effect and message for answer

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Question from "./Question";
+import "../styles/Quiz.css";
 
 function Quiz() {
   const [userAnswers, setUserAnswers] = useState(() => {
@@ -12,6 +13,9 @@ function Quiz() {
     return storedScore ? parseInt(storedScore, 10) : 0;
   });
 
+  const [scoreClass, setScoreClass] = useState("");
+  const [scoreText, setScoreText] = useState(`Score: ${score}`);
+
   useEffect(() => {
     sessionStorage.setItem("userAnswers", JSON.stringify(userAnswers));
     sessionStorage.setItem("score", score.toString());
@@ -22,6 +26,19 @@ function Quiz() {
     setUserAnswers(updatedAnswers);
     if (userAnswer === correctAnswer) {
       setScore(score + 1);
+      setScoreText("Correct! Nice one :^)");
+      setScoreClass("score-correct");
+      setTimeout(() => {
+        setScoreText(`Score: ${score}`);
+        setScoreClass("");
+      }, 1000);
+    } else {
+      setScoreText("WRONG! Unlucky 😭");
+      setScoreClass("score-wrong");
+      setTimeout(() => {
+        setScoreText(`Score: ${score}`);
+        setScoreClass("");
+      }, 1500);
     }
   };
 
@@ -29,7 +46,7 @@ function Quiz() {
     <div className="quiz">
       <div className="quiz-header">
         <h2>Is it a CAT or a DOG?</h2>
-        <p>Score: {score}</p>
+        <p className={scoreClass}>{scoreText}</p>
         <p>Questions Answered: {userAnswers.length}</p>
       </div>
       <Question onAnswer={handleAnswer} />
