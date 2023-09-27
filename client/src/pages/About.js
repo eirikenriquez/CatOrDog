855c01c9fd4d9c6bbe862426faa21bee4e4135b0
Eirik Enriquez <eirikdbbd@gmail.com>
2023-09-28 10:48:20 +1300

    updated about page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -17,57 +17,64 @@ function About() {
         and your task is to decide whether the image features a cat or a dog by
         clicking the corresponding button.
       </p>
-      <p>
-        As an animal lover, I found it exciting to work with free APIs that
-        provide an endless supply of cat and dog images for this project.
-      </p>
-      <p>The APIs used in this project are:</p>
-      <ul>
-        <li>
-          <a
-            href="https://thecatapi.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            The Cat API
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://dog.ceo/dog-api/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            The Dog API
-          </a>
-        </li>
-      </ul>
-
-      <p>
-        This project is a part of my journey in learning frontend development,
-        and it's hosted on GitHub. Feel free to explore the code and provide
-        feedback:
-      </p>
-      <ul>
-        <li>
-          <a
-            href="https://github.com/eirikenriquez"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Visit My GitHub Profile
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://github.com/eirikenriquez/CatOrDog"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Visit The GitHub Repo For This Site
-          </a>
-        </li>
-      </ul>
+      <h2>Links:</h2>
+      <div className="linksSection">
+        <div className="apiSection">
+          <h3>APIs used</h3>
+          <p>
+            As an animal lover, I found it exciting to work with free APIs that
+            provide an endless supply of cat and dog images for this project.
+          </p>
+          <ul>
+            <li>
+              <a
+                href="https://thecatapi.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                The Cat API
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://dog.ceo/dog-api/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                The Dog API
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="gitHubSection">
+          <h3>GitHub</h3>
+          <p>
+            This project is a part of my journey in learning frontend
+            development, and it's hosted on GitHub. Feel free to explore the
+            code and provide feedback.
+          </p>
+          <ul>
+            <li>
+              <a
+                href="https://github.com/eirikenriquez"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Visit My GitHub Profile
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/eirikenriquez/CatOrDog"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Visit The GitHub Repo For This Site
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
       <p>
         Thank you for playing and learning with me! Enjoy the quiz, and may your
         love for animals shine through.
